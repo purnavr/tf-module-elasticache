d22f933d0288590051f00ac66930f7ee64047d6b
purna <purnaraomba@gmail.com>
2024-03-29 12:15:06 +0530

    Roboshop Update

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+
+variable "engine" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "engine_version" {}
+variable "port" {}
