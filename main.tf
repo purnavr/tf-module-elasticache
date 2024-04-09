b4b4f176d67106a932c371f93110fb77ce5c0c89
purna <purnaraomba@gmail.com>
2024-04-09 23:47:37 +0530

    Roboshop Update

--- main.tf
+++ main.tf
@@ -6,12 +6,48 @@ resource "aws_elasticache_cluster" "elasticache" {
   engine_version       = var.engine_version
   port                 = var.port
   subnet_group_name = aws_elasticache_subnet_group.main.name
+  security_group_ids = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
     { Name = "${var.env}-elasticache" }
   )
 
+}
+
+resource "aws_security_group" "main" {
+  name        = "elasticache-${var.env}"
+  description = "elasticache-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "ELASTICACHE"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "elasticache-${var.env}" }
+  )
+}
+
+
+#resource "aws_vpc_security_group_ingress_rule" "ingress2" {
+#  security_group_id = aws_security_group.main.id
+#  cidr_ipv4         = var.allow_app_to
+#  from_port         = var.port
+#  ip_protocol       = "tcp"
+#  to_port           = var.port
+#  description = "APP"
+#}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.main.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_elasticache_subnet_group" "main" {
--- vars.tf
+++ vars.tf
@@ -7,3 +7,6 @@ variable "node_type" {}
 variable "num_cache_nodes" {}
 variable "engine_version" {}
 variable "port" {}
+variable "vpc_id" {}
+variable "allow_subnets" {}
+
