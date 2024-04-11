48f6db14f502a10f53287a7efd7990caba113891
purna <purnaraomba@gmail.com>
2024-04-11 12:13:09 +0530

    Roboshop Update

--- vars.tf
+++ vars.tf
@@ -1,12 +1,14 @@
 variable "env" {}
 variable "tags" {}
+
 variable "subnet_ids" {}
+variable "vpc_id" {}
 
 variable "engine" {}
 variable "node_type" {}
 variable "num_cache_nodes" {}
 variable "engine_version" {}
 variable "port" {}
-variable "vpc_id" {}
 variable "allow_subnets" {}
+
 
